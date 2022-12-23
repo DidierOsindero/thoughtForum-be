f84d555cdd907dc9837a6d5c80dbf8f4f62957cd
Didier Osindero <didierosindero@gmail.com>
2022-12-23 13:01:08 +0100

    Connect to database and create add post to DB

--- src/db.ts
+++ src/db.ts
@@ -1,125 +1,138 @@
+import { Client } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+
 export interface DbItem {
-  // sketch out interface here
+  user_id: string;
+  post_id: string;
+  img: string;
+  title: string;
+  content: string;
+  type: PostType;
+  privacy: PostPrivacy;
+  hearts: number;
+  creation_date: string;
 }
 
-export interface DbItemWithId extends DbItem {
-  id: number;
+export type PostType = "thought" | "science" | "art";
+export type PostPrivacy = "public" | "private";
+
+export interface INewPostData {
+  img: string | null;
+  title: string;
+  content: string;
+  type: PostType | null;
+  privacy: PostPrivacy | null;
 }
 
-const db: DbItemWithId[] = [];
+const db: DbItem[] = [
+  {
+    user_id: "1",
+    post_id: "1",
+    title: "Why do we think the way we do?",
+    content:
+      "In their most common sense, the terms thought and thinking refer to conscious cognitive processes that can happen independently of sensory stimulation. Their most paradigmatic forms are judging, reasoning, concept formation, problem solving, and deliberation. But other mental processes, like considering an idea, memory, or imagination, are also often included. These processes can happen internally independent of the sensory organs, unlike perception.",
+    img: "https://images.unsplash.com/photo-1617791160536-598cf32026fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80",
+    type: "science",
+    privacy: "public",
+    hearts: 0,
+    creation_date: "January 21 2023",
+  },
+  {
+    user_id: "1",
+    post_id: "2",
+    title: "Why I'm scared of the future",
+    content:
+      "Fear is an intensely unpleasant emotion in response to perceiving or recognizing a danger or threat. Fear causes physiological changes that may produce behavioral reactions such as mounting an aggressive response or fleeing the threat. Fear in human beings may occur in response to a certain stimulus occurring in the present, or in anticipation or expectation of a future threat perceived as a risk to oneself.Fear is closely related to the emotion anxiety, which occurs as the result of threats that are perceived to be uncontrollable or unavoidable.[1] The fear response serves survival by engendering appropriate behavioral responses, so it has been preserved throughout evolution.[2] Sociological and organizational research also suggests that individuals' fears are not solely dependent on their nature but are also shaped by their social relations and culture, which guide their understanding of when and how much fear to feel.",
+    img: "https://images.unsplash.com/photo-1670772295035-6baf4bf424ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMyfHFQWXNEenZKT1ljfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    type: "thought",
+    privacy: "public",
+    hearts: 0,
+    creation_date: "January 21 2023",
+  },
+  {
+    user_id: "1",
+    post_id: "3",
+    title: "When magic is deceptive",
+    content:
+      "In their most common sense, the terms thought and thinking refer to conscious cognitive processes that can happen independently of sensory stimulation. Their most paradigmatic forms are judging, reasoning, concept formation, problem solving, and deliberation. But other mental processes, like considering an idea, memory, or imagination, are also often included. These processes can happen internally independent of the sensory organs, unlike perception.",
+    img: "https://images.unsplash.com/photo-1670787196366-11b4a82501e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfHFQWXNEenZKT1ljfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    type: "science",
+    privacy: "public",
+    hearts: 0,
+    creation_date: "January 22 2023",
+  },
+  {
+    user_id: "2",
+    post_id: "4",
+    title: "Hubris in Hamlet",
+    content:
+      "The Tragedy of Hamlet, Prince of Denmark, often shortened to Hamlet (/ˈhæmlɪt/), is a tragedy written by William Shakespeare sometime between 1599 and 1601. It is Shakespeare's longest play, with 29,551 words. Set in Denmark, the play depicts Prince Hamlet and his attempts to exact revenge against his uncle, Claudius, who has murdered Hamlet's father in order to seize his throne and marry Hamlet's mother.",
+    img: "https://images.unsplash.com/photo-1553782749-5ab8693a5f4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hha2VzcGVhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    type: "art",
+    privacy: "public",
+    hearts: 0,
+    creation_date: "January 23 2023",
+  },
+  {
+    user_id: "3",
+    post_id: "5",
+    title: "The beauty of silence",
+    content:
+      "In their most common sense, the terms thought and thinking refer to conscious cognitive processes that can happen independently of sensory stimulation. Their most paradigmatic forms are judging, reasoning, concept formation, problem solving, and deliberation. But other mental processes, like considering an idea, memory, or imagination, are also often included. These processes can happen internally independent of the sensory organs, unlike perception.",
+    img: "https://images.unsplash.com/photo-1671557488735-1c5d99cfe045?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+    type: "thought",
+    privacy: "public",
+    hearts: 0,
+    creation_date: "January 24 2023",
+  },
+];
 
-/** Variable to keep incrementing id of database items */
-let idCounter = 0;
-
-/**
- * Adds in some dummy database items to the database
- *
- * @param n - the number of items to generate
- * @returns the created items
- */
-export const addDummyDbItems = (n: number): DbItemWithId[] => {
-  const createdSignatures: DbItemWithId[] = [];
-  for (let count = 0; count < n; count++) {
-    const createdSignature = addDbItem({
-      // possibly add some generated data here
-    });
-    createdSignatures.push(createdSignature);
-  }
-  return createdSignatures;
-};
-
-/**
- * Adds in a single item to the database
- *
- * @param data - the item data to insert in
- * @returns the item added (with a newly created id)
- */
-export const addDbItem = (data: DbItem): DbItemWithId => {
-  const newEntry: DbItemWithId = {
-    id: ++idCounter,
-    ...data,
-  };
-  db.push(newEntry);
-  return newEntry;
-};
-
-/**
- * Deletes a database item with the given id
- *
- * @param id - the id of the database item to delete
- * @returns the deleted database item (if originally located),
- *  otherwise the string `"not found"`
- */
-export const deleteDbItemById = (id: number): DbItemWithId | "not found" => {
-  const idxToDeleteAt = findIndexOfDbItemById(id);
-  if (typeof idxToDeleteAt === "number") {
-    const itemToDelete = getDbItemById(id);
-    db.splice(idxToDeleteAt, 1); // .splice can delete from an array
-    return itemToDelete;
-  } else {
-    return "not found";
-  }
-};
-
-/**
- * Finds the index of a database item with a given id
- *
- * @param id - the id of the database item to locate the index of
- * @returns the index of the matching database item,
- *  otherwise the string `"not found"`
- */
-const findIndexOfDbItemById = (id: number): number | "not found" => {
-  const matchingIdx = db.findIndex((entry) => entry.id === id);
-  // .findIndex returns -1 if not located
-  if (matchingIdx !== -1) {
-    return matchingIdx;
-  } else {
-    return "not found";
-  }
-};
-
-/**
- * Find all database items
- * @returns all database items from the database
- */
-export const getAllDbItems = (): DbItemWithId[] => {
+export const getAllPosts = async () => {
   return db;
 };
 
-/**
- * Locates a database item by a given id
- *
- * @param id - the id of the database item to locate
- * @returns the located database item (if found),
- *  otherwise the string `"not found"`
- */
-export const getDbItemById = (id: number): DbItemWithId | "not found" => {
-  const maybeEntry = db.find((entry) => entry.id === id);
-  if (maybeEntry) {
-    return maybeEntry;
-  } else {
-    return "not found";
+export const addNewPost = async (newPostData: INewPostData, userId: string) => {
+  try {
+    const queryText =
+      "INSERT INTO user_posts (user_id, title, content, img, category, privacy) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
+    const queryValues = [
+      userId,
+      newPostData.title,
+      newPostData.content,
+      newPostData.img,
+      newPostData.type,
+      newPostData.privacy,
+    ];
+    const queryResponse = await client.query(queryText, queryValues);
+    const createdPost = queryResponse.rows[0];
+
+    return createdPost;
+  } catch (error) {
+    console.error(
+      "There was an error when adding a new post to the database: ",
+      error
+    );
   }
 };
 
-/**
- * Applies a partial update to a database item for a given id
- *  based on the passed data
- *
- * @param id - the id of the database item to update
- * @param newData - the new data to overwrite
- * @returns the updated database item (if one is located),
- *  otherwise the string `"not found"`
- */
-export const updateDbItemById = (
-  id: number,
-  newData: Partial<DbItem>
-): DbItemWithId | "not found" => {
-  const idxOfEntry = findIndexOfDbItemById(id);
-  // type guard against "not found"
-  if (typeof idxOfEntry === "number") {
-    return Object.assign(db[idxOfEntry], newData);
-  } else {
-    return "not found";
-  }
-};
+// /** Variable to keep incrementing id of database items */
+// let idCounter = 0;
+
+// /**
+//  * Adds in a single item to the database
+//  *
+//  * @param data - the item data to insert in
+//  * @returns the item added (with a newly created id)
+//  */
+// export const addDbItem = (data: DbItem): DbItemWithId => {
+//   const newEntry: DbItemWithId = {
+//     id: ++idCounter,
+//     ...data,
+//   };
+//   db.push(newEntry);
+//   return newEntry;
+// };
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {
-  addDummyDbItems,
-  addDbItem,
-  getAllDbItems,
-  getDbItemById,
-  DbItem,
-  updateDbItemById,
-} from "./db";
+import { addNewPost, getAllPosts, INewPostData } from "./db";
 import filePath from "./filePath";
 
-// loading in some dummy items into the database
-// (comment out if desired, or change the number)
-addDummyDbItems(20);
-
 const app = express();
-
-/** Parses JSON data in a request automatically */
 app.use(express.json());
-/** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
 app.use(cors());
-
-// read in contents of any environment variables in the .env file
 dotenv.config();
-
-// use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
+
+//============GET============
 
 // API info page
 app.get("/", (req, res) => {
@@ -34,48 +18,35 @@ app.get("/", (req, res) => {
   res.sendFile(pathToFile);
 });
 
-// GET /items
-app.get("/items", (req, res) => {
-  const allSignatures = getAllDbItems();
-  res.status(200).json(allSignatures);
+app.get("/posts", async (req, res) => {
+  const posts = await getAllPosts();
+  res.json(posts);
 });
 
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbItem(postData);
-  res.status(201).json(createdSignature);
+app.get("/thought", (req, res) => {
+  const pathToFile = filePath("../public/index.html");
+  res.sendFile(pathToFile);
 });
 
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
+app.get("/science", (req, res) => {
+  const pathToFile = filePath("../public/index.html");
+  res.sendFile(pathToFile);
 });
 
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
+app.get("/art", (req, res) => {
+  const pathToFile = filePath("../public/index.html");
+  res.sendFile(pathToFile);
 });
 
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
+//============POST============
+app.post<{}, {}, INewPostData>("/write", async (req, res) => {
+  if (req.headers.authorization) {
+    const createdPost = await addNewPost(req.body, req.headers.authorization);
+    if (createdPost) {
+      res.json(createdPost);
+    } else {
+      res.status(400);
+    }
   }
 });
 
